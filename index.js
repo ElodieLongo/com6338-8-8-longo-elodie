ab7cd6f63588306047aa0142aa6e7da5ed51180a
ElodieLongo <mail.elodielongo@gmail.com>
2023-10-27 17:09:46 +0200

    API call added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-document.getElementbyId (weather-search)
+
+
+
+
+
+/* API openweather key: 5e62588767f708095f07c8b5c02e7660
+
+https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={5e62588767f708095f07c8b5c02e7660}
+
+
+*/
+
+let weatherApp = document.getElementById ("weather-app")
+let Search = document.getElementbyId ("weather-search")
+
+function weatherApp() {
+
+}
+
+function weatherAppLoad() {
+
+}
