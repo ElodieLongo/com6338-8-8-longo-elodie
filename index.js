4348bdcf27f58eba9b5270cfb7e0d640030d566e
ElodieLongo <mail.elodielongo@gmail.com>
2023-10-30 16:42:24 +0100

    assignment 8

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let apiUrl = "https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={l
 
 const geocodingURL = "https://api.openweathermap.org/geo/1.0/direct?q=$locationInput&limit&=1units=imperial&appid=${apiKey}";
 
-const locationElement = document.querySelector("#weather h2");
+const weatherElement = document.querySelector("#weather");
+ /*
 const googleMapElement = document.querySelector("#weather a");
 const weatherIconElement = document.querySelector("#weather img");
-const currentWeatherDescriptionElement = document.querySelector("#weather p:nth-of-type(1)");
+  */
+const currentWeatherDescriptionElement = document.querySelector("#weather p:first-child");
 const currentTempElement = document.querySelector("#weather p:nth-of-type(2)");
 const feelsLikeTempElement = document.querySelector("#weather p:nth-of-type(3)");
 const lastUpdatedTimeElement = document.querySelector("#weather p:last-child");
@@ -16,35 +18,36 @@ const locationInput = document.getElementById("weather-search");
 
 searchButton.addEventListener("click", async function (event) {
     event.preventDefault();
-    const userQuery = locationInput.value;
-    if (userQuery) {
-      try {
-        // Fetch geocoding data to get latitude and longitude
-        const geocodingResponse = await fetch(geocodingURL.replace("{location}", userQuery).replace("{apiKey}", apiKey));
-        const geocodingData = await geocodingResponse.json();
-  
-        if (geocodingData.cod === 200) {
-          const lat = geocodingData.coord.lat;
-          const lon = geocodingData.coord.lon;
-  
-          // Fetch weather data using latitude and longitude
-          const weatherURL = apiUrl.replace("{lat}", lat).replace("{lon}", lon).replace("{time}", "current").replace("{apiKey}", apiKey);
-          const weatherResponse = await fetch(weatherURL);
-          const weatherData = await weatherResponse.json();
-  
-          // Update the HTML elements with weather data
-          locationElement.textContent = `${geocodingData.name}, ${geocodingData.sys.country}`;
-          googleMapElement.href = `https://www.google.com/maps/search/?api=1&query=${lat},${lon}`;
-          weatherIconElement.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-          currentWeatherDescriptionElement.textContent = weatherData.weather[0].description;
-          currentTempElement.textContent = `Current: ${weatherData.main.temp}° F`;
-          feelsLikeTempElement.textContent = `Feels like: ${weatherData.main.feels_like}° F`;
-          lastUpdatedTimeElement.textContent = `Last updated: ${new Date().toLocaleTimeString()}`;
-        } else {
-          console.error("City not found");
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-  });
+    const userInput = locationInput.value;
+   
+    const geocodingResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&limit=1&units=imperial&appid=${apiKey}`)
+    const geocodingData = await geocodingResponse.json();
+    locationInput.value = '';
+if (geocodingData.cod == "404"){
+  weatherElement.innerHTML = `<h2>Location Not Found</h2>`
+  console.log(weatherElement)
+  return
+}
+
+console.log(geocodingData)
+
+    const date = new Date(geocodingData.dt*1000)
+    const timeString = date.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit'
+    })
+
+
+
+weatherElement.innerHTML = `<h2>${geocodingData.name}, ${geocodingData.sys.country}</h2>
+<a href="https://www.google.com/maps/search/?api=1&query=${geocodingData.coord.lat},${geocodingData.coord.lon}"
+target="__BLANK">Click to view map</a>
+<img src="https://openweathermap.org/img/wn/${geocodingData.weather[0].icon}@2x.png">
+<p style="text-transform: capitalize;">${geocodingData.weather[0].description}</p><br>
+<p>Current: ${geocodingData.main.temp}° F</p>
+<p>Feels like: ${geocodingData.main.feels_like}° F</p><br>
+<p>Last updated: ${timeString}</p>`
+
+ // 
+
+});
